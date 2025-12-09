1b837408be4c259f4fe104a29f510f11787ab240
Ashique <ashique9496@gmail.com>
2025-12-09 11:45:13 +0530

    Implemented Lenis for smooth scrolling

--- frontend/src/components/SmoothScrolling.jsx
+++ frontend/src/components/SmoothScrolling.jsx
@@ -1,9 +1,28 @@
 "use client";
 import { ReactLenis } from "lenis/react";
+import { useRef, useEffect } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 function SmoothScrolling({ children }) {
+    const lenisRef = useRef();
+
+    useEffect(() => {
+        function update(time) {
+            lenisRef.current?.lenis?.raf(time * 1000);
+        }
+
+        gsap.ticker.add(update);
+
+        return () => {
+            gsap.ticker.remove(update);
+        };
+    }, []);
+
     return (
-        <ReactLenis root options={{ lerp: 0.05, duration: 2, smoothWheel: true }}>
+        <ReactLenis root ref={lenisRef} autoRaf={false} options={{ lerp: 0.05, duration: 2, smoothWheel: true }}>
             {children}
         </ReactLenis>
     );
